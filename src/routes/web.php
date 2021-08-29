18a1ee0ac8b4a6eee16286e1c1bb077256bf8767
Isma Rianto <ismarianto97@gmail.com>
2021-08-30 00:52:11 +0700

    update repo

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,11 +3,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('indexapp::index');
+Route::get('', function () {
+    return view('ismarianto::indexapp.index');
 });
 
 Route::group(['namespace' => 'Ismarianto\Ismarianto\Controllers', 'middleware' => ['web']], function () {
-    // Route::get('/', 'IndexaplikasiController@index')->name('/');
+    Route::get('/', 'IndexaplikasiController@index')->name('/');
     // Route::get('/', 'IndexaplikasiController@index')->name('');
  });
